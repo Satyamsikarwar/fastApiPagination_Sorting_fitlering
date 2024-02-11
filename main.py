5c1edd28939b2b578716682899d8f8872978ca12
Satyamsikarwar <satyamsikarwar343@gmail.com>
2024-02-12 01:54:15 +0530

    changed uri string

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://satyam:satyam@cluster0.dtj8fle.mongodb.net/?retryWrites=true&w=majority"
+uri = "xyz"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 db = client['usersDb']
